30cb03a4bf8a5d44e7a0f2f95c71352daf443bbf
Yesid Leonardo Lopez Sierra <ylopezs@pslcol.com.co>
2019-02-05 15:59:45 -0500

    add RESTful API

--- Express/app.js
+++ Express/app.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var fs = require('fs');
 var app = express();
+var bodyParser = require('body-parser');
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+
 
 // Find the enviroment variable called PORT, else 3000 is the port
 var port = process.env.PORT || 3000
@@ -24,6 +32,26 @@ app.get('/', function(req, res){
 // Using params and ejs to show them in the view
 app.get('/person/:id', function(req, res){
     res.render('person', { ID: req.params.id })
+});
+
+// Using params and ejs to show them in the view but with Post methods
+app.post('/person', urlencodedParser, function(req, res){
+    res.send('Data sent');
+    console.log(req.body.firstname);
+    console.log(req.body.lastname);
+});
+
+// RESTful API
+app.get('/api/person/:id', function(req, res){
+    //get data from the database
+});
+
+app.post('/api/person', jsonParser, function(req, res){
+    //Create or save to the database
+});
+
+app.delete('/api/person/:id', function(req, res){
+    //Delete to the database
 });
 
 // Find html without use the view folder
