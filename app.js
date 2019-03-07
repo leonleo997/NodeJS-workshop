803830d3a581d20008cad859d7fd7dde4c7a4223
leonleo997 <yesid.lopez@correo.icesi.edu.co>
2019-03-06 20:53:14 -0500

    making application

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 var Express = require('express');
 var app = Express();
+var serverPeople = this.people = [
+    {
+        name:'Yesid'
+    },
+    {
+        name:'Yesid'
+    },
+    {
+        name:'Yesid'
+    }
+];
 
 app.set('view engine','ejs');
 app.use('/assets', Express.static(__dirname + '/public'));
 
 app.get('/', function(req,res) {
-    res.render('index');
+    res.render('index', serverPeople);
 })
 
 var PORT = process.env.PORT || 3000
--- public/js/app.js
+++ public/js/app.js
@@ -4,4 +4,15 @@ angular.module('TestApp').controller('MainController', ctrlFunc);
 
 function ctrlFunc(){
     this.message = 'hello';
+    this.people = [
+        {
+            name:'Yesid'
+        },
+        {
+            name:'Yesid'
+        },
+        {
+            name:'Yesid'
+        }
+    ]
 }
